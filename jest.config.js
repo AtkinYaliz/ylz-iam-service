6d32309fe074fa078c34eacccbec0eb13df84b09
Aytekin Yaliz <aytekin.yaliz@dentsuaegis.com>
2018-09-30 20:26:39 +0100

    initials

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+   globals: {
+      "ts-jest": {
+         tsConfigFile: "tsconfig.json"
+      }
+   },
+   moduleFileExtensions: ["ts", "js"],
+   transform: {
+      "^.+\\.tsx?$": "ts-jest",
+      "^.+\\.js$": "babel-jest"
+   },
+   transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$"],
+   testMatch: ["**/test/**/*.(test|spec).(ts|js)"],
+   testEnvironment: "node",
+   testResultsProcessor: "jest-sonar-reporter",
+   collectCoverageFrom: ["src/controllers/**/*.{ts}"],
+   verbose: true
+};
