a299ce310439f754c2c696241b08464df0b7aec3
omerfsen <omerfsen@gmail.com>
2018-12-10 15:33:36 +0000

    add mongors shared-bootstrap create script

---
+++ k8s/mongors/create_keyfile.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+TMPFILE=$(mktemp)
+/usr/bin/openssl rand -base64 741 > $TMPFILE
+kubectl create secret generic shared-bootstrap-data --from-file=internal-auth-mongodb-keyfile=$TMPFILE
+rm $TMPFILE
+
+
